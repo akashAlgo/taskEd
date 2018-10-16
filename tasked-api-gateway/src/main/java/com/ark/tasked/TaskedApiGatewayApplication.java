88971ed4d5483f264d8a601f16320866d1bda061
Akash <akash@algorythma.com>
2018-10-16 22:10:56 +0400

    Adding api-gateway microservice skeleton.

---
+++ tasked-api-gateway/src/main/java/com/ark/tasked/TaskedApiGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.ark.tasked;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskedApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskedApiGatewayApplication.class, args);
+	}
+}
