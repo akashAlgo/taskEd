bfbce2782603f2f40d02926fcfe652fe0d9aefbf
Akash <akash@algorythma.com>
2018-10-16 22:07:04 +0400

    Adding tasked-service microservice skeleton to the repo.

---
+++ tasked-service/src/main/java/com/ark/tasked/TaskedServiceApplication.java
@@ -0,0 +1,12 @@
+package com.ark.tasked;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskedServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskedServiceApplication.class, args);
+	}
+}
